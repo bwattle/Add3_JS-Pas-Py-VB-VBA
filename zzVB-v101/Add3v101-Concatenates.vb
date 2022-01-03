b702b37895c0e1c1b94609aa1587954a52333e3e
Nev Goodyer <neville.goodyer@det.nsw.edu.au>
2022-01-03 16:04:41 +1100

    Cleanup for VS2022

--- zzVB-v101/Add3v101-Concatenates.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Add3v101
-    Private Sum As Integer
-    Private Sub btnCalcSum_Click(sender As Object, e As EventArgs) Handles btnCalcSum.Click
-        Sum = txtNo1.Text + txtNo2.Text + txtNo3.Text
-        txtSumValue.Text = Sum
-    End Sub
-End Class
